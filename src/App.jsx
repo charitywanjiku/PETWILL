0a0f653359a62361711074607d848921548878bd
victormuteithia <muteithiavictor@gmail.com>
2024-02-14 15:50:15 +0300

    Updated form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import Pages from './components/Pages';
+import Form from './components/Form';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App () {
   return (
     <>
-        <Pages />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/form" element={<Form />}/>
+    </Routes>
+    </BrowserRouter>
+    <Form />
     </>
 
     
