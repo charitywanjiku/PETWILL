46a1e27ebd3ea52acec91ff904b53cb5cc638672
victormuteithia <muteithiavictor@gmail.com>
2024-02-14 19:15:39 +0300

    Resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
 import Form from './components/Form';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import TeamTable from './components/TeamTable';
 
 function App () {
   return (
     <>
-    <BrowserRouter>
     <Routes>
       <Route path="/Team" element={<TeamTable/>}/>
       <Route path="/Profile" element={<Form />}/>
     </Routes>
-    </BrowserRouter>
-    <Form />
     </>
 
     
